b3e98c72505dba2d1241c3d3e7c8869ae681de8f
MarinuIvanova <marinulkagubina@gmail.com>
2025-10-26 16:08:08 +0700

    sql tests

--- cypress/e2e/sql.cy.js
+++ cypress/e2e/sql.cy.js
@@ -1,11 +1,38 @@
-
 //password: test$12345$mg$
 
-describe('connect to test db', () => {
-  it('can connect to db', () => {
-    cy.task("queryDb", "CREATE TABLE Students (StudentID int, FirstName varchar(255), StudentGroup varchar(255), City varchar(255))")
-  })
-  it("delete the db", ()=>{
-    cy.task("queryDb", "DROP TABLE Students")
-  })
-})
+describe("connect to test db", () => {
+  it("can connect to db", () => {
+    cy.task(
+      "queryDb",
+      "CREATE TABLE Students (StudentID int, FirstName varchar(255), StudentGroup varchar(255), City varchar(255))"
+    );
+  });
+
+  it("input entries", () => {
+    cy.task(
+      "queryDb",
+      `INSERT INTO Students (StudentID, FirstName, StudentGroup, City) VALUES
+      (1, "Ivan", "02-2025", "Russia"),
+      (2, "Simon", "04-2025", "Tokio"),
+      (3, "Peter", "05-2025", "China")
+      `
+    ).then((result) => {
+      cy.log(JSON.stringify(result));
+      expect(result.affectedRows).to.equal(3);
+    });
+  });
+
+  it("select", () => {
+    cy.task(
+      "queryDb",
+      `SELECT FirstName FROM Students WHERE City="China"`
+    ).then((result) => {
+      cy.log(JSON.stringify(result));
+      expect(result[0].FirstName).to.equal("Peter");
+    });
+  });
+
+  it("delete the db", () => {
+    cy.task("queryDb", "DROP TABLE Students");
+  });
+});
